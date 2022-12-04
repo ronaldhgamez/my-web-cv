c93948bd03d0ad049852d816f5e25be9862d22cb
ronaldhgamez <ronaldhgamez@gmail.com>
2022-12-04 00:06:01 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PizzaComponent } from './pizza/pizza.component';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pizza', pathMatch: 'full' },
+  { path: 'pizza', component: PizzaComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
